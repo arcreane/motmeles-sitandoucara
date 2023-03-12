3bc49d4e080569618e58f67e04b97a316a10c951
Sitan Doucara <doucarasitan@gmail.com>
2023-03-12 15:31:19 +0100

    creation of a function to display the grid

--- projet.py
+++ projet.py
@@ -8,7 +8,23 @@ word_list = ["colère", "dallai", "marbre", "normes","solive","trésor"]
 line = 15
 column = 7
 
-#random letter generator based on column and row
-grid = [[random.choice(string.ascii_uppercase) for i in range(0,line)] for j in range(0,column)]
-print("\n".join(map(lambda row: "  ".join(row), grid)))
-print("Select Column : 7 \nSelect Line : 15")
+#Random letter generator based on column and line
+letters = [[random.choice(string.ascii_uppercase) for i in range(0,column)] for j in range(0,line)]
+
+#Define function to display grid
+#Using f-string(f') for concatenation and a 2D array (2d) for number alignment
+def display_grid():
+    # Display column numbers 
+    print('  ', end='  ')
+    for i in range(0,column):
+        print(f'{i:2d}', end=' ')
+    print('')
+    # Display top line
+    print('  _|' + ' _'*(column+4) + '|_')
+    # Display grid line with line numbers
+    for i in range(0,line):
+        print(f'{i:2d} |', end=' ')
+        for j in range(0,column):
+            print(letters[i][j], end='  ')
+        print('|')
+display_grid()
